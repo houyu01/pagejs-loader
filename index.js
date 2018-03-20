ef2fdda29729764c9f52761b6453f47851a88f99
houyu01 <houyu01@baidu.com>
2018-03-20 11:23:08 +0800

    [fix a bug on windows]

--- index.js
+++ index.js
@@ -4,9 +4,9 @@
  */
 module.exports = function (source, other) {
     var path = /(pages.*).js$/;
-    var queryPath = path.exec(this.resourcePath);
-    queryPath = queryPath ? queryPath[1] : '';
-    var startTpl = `PageDefine('${queryPath}', function (Page, getApp) {`;
+    var queryPath = path.exec(this.resourcePath.replace(/\\/ig, '/'));
+	queryPath = queryPath ? queryPath[1] : '';
+    var startTpl = `PageDefine('${queryPath}', function (Page, swan, getApp) {`;
     var endTpl = `});`;
     return startTpl + source + endTpl;
 };
