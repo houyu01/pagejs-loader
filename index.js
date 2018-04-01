270c5c7c963dc1566e6cc470f5d8c296065556f9
houyu01 <houyu01@baidu.com>
2018-04-02 00:07:41 +0800

    [调整define]

--- index.js
+++ index.js
@@ -3,10 +3,20 @@
  * @author houyu(785798835@qq.com)
  */
 module.exports = function (source, other) {
-    var path = /(pages.*).js$/;
-    var queryPath = path.exec(this.resourcePath.replace(/\\/ig, '/'));
-	queryPath = queryPath ? queryPath[1] : '';
-    var startTpl = `PageDefine('${queryPath}', function (Page, swan, getApp) {`;
-    var endTpl = `});`;
+    var query = this.query.replace('?', '');
+    var configPath = query.replace('configPath=', '');
+    var config = require(configPath);
+    var path = /workspace\/(.*).js$/;
+    var queryStructure = path.exec(this.resourcePath.replace(/\\/ig, '/'));
+    var queryPath = '';
+    if (queryStructure) {
+        queryPath = queryStructure[1];
+    }
+    else {
+        return source;
+    }
+    var supplement = !!~config.pages.indexOf(queryPath) ? `window.require('${queryPath}');` : '';
+    var startTpl = `window.define('${queryPath}', function (require, module, exports, define, Page, swan, getApp, window, document, frames, self, location, navigator, localStorage, history, Caches) {`;
+    var endTpl = `});${supplement}`;
     return startTpl + source + endTpl;
 };
